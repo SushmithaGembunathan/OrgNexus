85a09c139087467b12783fe9a78fc593ab306a5f
SushmithaGembunathan <mathusush1106@gmail.com>
2026-03-15 22:17:29 +0530

    Improved chatbot using NLP similarity with spaCy

--- backend/chatbot_engine.py
+++ backend/chatbot_engine.py
@@ -1,14 +1,29 @@
 import json
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
 
 with open("data/knowledge_base.json") as f:
     knowledge = json.load(f)
 
 def get_response(user_input):
 
-    user_input = user_input.lower()
+    user_doc = nlp(user_input)
 
-    for key in knowledge:
-        if key in user_input:
-            return knowledge[key]
+    best_match = None
+    highest_similarity = 0
 
-    return "Sorry, I don't have information about that yet."
+    for question in knowledge:
+
+        question_doc = nlp(question)
+
+        similarity = user_doc.similarity(question_doc)
+
+        if similarity > highest_similarity:
+            highest_similarity = similarity
+            best_match = question
+
+    if highest_similarity > 0.5:
+        return knowledge[best_match]
+
+    return "Sorry, I couldn't understand that. Please rephrase your question."
